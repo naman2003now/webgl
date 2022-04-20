7337b517eb5994834ae2e1152ba7d9d7a5f7972d
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-04-20 21:41:03 +0530

    Done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var canvas = document.getElementById("canvas");
-canvas.height = 2000;
-canvas.width = 2000;
+canvas.height = 3000;
+canvas.width = 3000;
 var gl = canvas.getContext("webgl");
+canvas.height = 1000;
+canvas.width = 1000;
 
 canvas.style.height = Math.min(window.innerHeight, window.innerWidth) + "px";
 canvas.style.width = Math.min(window.innerHeight, window.innerWidth) + "px";
@@ -31,12 +33,8 @@ gl.shaderSource(
 	vertexShader,
 	`
     attribute vec3 aPosition;
-    attribute vec3 aColor;
-
-    varying vec4 vColor;
 
     void main(){
-        vColor = vec4(aColor, 1);
         gl_Position = vec4(aPosition, 1.0);
     }
 `
@@ -47,20 +45,36 @@ const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(
 	fragmentShader,
 	`
-    precision mediump float;
+    precision lowp int;
+    precision highp float;
 
-    varying vec4 vColor;
+    uniform float x_min;
+    uniform float x_max;
+    uniform float y_min;
+    uniform float y_max;
 
-    uniform vec2 uMouse;
-    uniform float uTime;
+    uniform vec2 u_resolution;
 
     void main(){
-        float d = length(gl_FragCoord.xy - uMouse);
-        float r = 500.0;
-        if(d < r){
-            gl_FragColor = vec4(d*vec3(1, 1, 1)/r, 0.0);
-        }else{
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        float x0 = x_min + (x_max - x_min)*(gl_FragCoord.x / u_resolution.x);
+        float y0 = y_min + (y_max - y_min)*(gl_FragCoord.y / u_resolution.y);
+        float x = 0.0;
+        float y = 0.0;
+        int found = 0;
+        const int max_iteration = 100;
+        for(int k = 0; k < max_iteration; k++){
+            float x_new = x*x - y*y + x0;
+            float y_new = 2.0*x*y + y0;
+            if(x_new*x_new + y_new*y_new > 4.0){
+                found = 1;
+                gl_FragColor = vec4(k, k, 0.1, 100)/100.0;
+                break;
+            }
+            x = x_new;
+            y = y_new;
+        }
+        if(found == 0){
+            gl_FragColor = vec4(0, 0, 0, 1);
         }
     }
 `
@@ -82,39 +96,88 @@ gl.vertexAttribPointer(
 	6 * Float32Array.BYTES_PER_ELEMENT,
 	0
 );
-const colorAttribute = gl.getAttribLocation(program, "aColor");
-gl.enableVertexAttribArray(colorAttribute);
-gl.vertexAttribPointer(
-	colorAttribute,
-	3,
-	gl.FLOAT,
-	false,
-	6 * Float32Array.BYTES_PER_ELEMENT,
-	3 * Float32Array.BYTES_PER_ELEMENT
-);
 
 gl.useProgram(program);
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-var x;
-var y;
-window.addEventListener("mousemove", function (e) {
-	x = e.clientX;
-	y = e.clientY;
-
-	x -= canvas.offsetLeft;
-	y -= canvas.offsetTop;
-
-	x *= canvas.width / canvas.clientWidth;
-	y *= canvas.height / canvas.clientHeight;
-	y = canvas.height - y;
-});
+console.log(gl.getShaderInfoLog(fragmentShader));
 
 const mouseUniformLocation = gl.getUniformLocation(program, "uMouse");
 const timeUniformLocation = gl.getUniformLocation(program, "uTime");
 
+const x_min = gl.getUniformLocation(program, "x_min");
+const x_max = gl.getUniformLocation(program, "x_max");
+const y_min = gl.getUniformLocation(program, "y_min");
+const y_max = gl.getUniformLocation(program, "y_max");
+
+const resolutionUniformLocation = gl.getUniformLocation(
+	program,
+	"u_resolution"
+);
+
+gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+
+document.getElementById("resolution").addEventListener("change", (e) => {
+	const resolution = e.target.value;
+	document.getElementById("resolutionDisplay").innerHTML = resolution;
+	canvas.height = resolution;
+	canvas.width = resolution;
+	gl.uniform2f(resolutionUniformLocation, resolution, resolution);
+});
+
+var moveDelta = 0.1;
+
+window.addEventListener("keydown", (e) => {
+	if (e.key === "ArrowLeft" || e.key === "a") {
+		x_min_value -= moveDelta;
+		x_max_value -= moveDelta;
+	} else if (e.key === "ArrowRight" || e.key === "d") {
+		x_min_value += moveDelta;
+		x_max_value += moveDelta;
+	} else if (e.key === "ArrowUp" || e.key === "w") {
+		y_min_value += moveDelta;
+		y_max_value += moveDelta;
+	} else if (e.key === "ArrowDown" || e.key === "s") {
+		y_min_value -= moveDelta;
+		y_max_value -= moveDelta;
+	} else if (e.key === "q") {
+		let scalex = (x_max_value - x_min_value) * 0.8;
+		let scaley = (y_max_value - y_min_value) * 0.8;
+
+		moveDelta -= 0.02;
+
+		let temp_x = x_max_value;
+		let temp_y = y_max_value;
+
+		x_max_value = x_min_value + scalex;
+		x_min_value = temp_x - scalex;
+
+		y_max_value = y_min_value + scaley;
+		y_min_value = temp_y - scaley;
+	} else if (e.key === "e") {
+		let scalex = (x_max_value - x_min_value) * 1.2;
+		let scaley = (y_max_value - y_min_value) * 1.2;
+
+		moveDelta += 0.02;
+
+		let temp_x = x_max_value;
+		let temp_y = y_max_value;
+
+		x_max_value = x_min_value + scalex;
+		x_min_value = temp_x - scalex;
+
+		y_max_value = y_min_value + scaley;
+		y_min_value = temp_y - scaley;
+	}
+});
+var x_min_value = -2.0;
+var x_max_value = 0.47;
+var y_min_value = -1.12;
+var y_max_value = 1.12;
+
 var renderLoop = setInterval(() => {
-	gl.uniform1f(timeUniformLocation, performance.now() / 1000);
-	gl.uniform2f(mouseUniformLocation, x, y);
+	gl.uniform1f(x_min, x_min_value);
+	gl.uniform1f(x_max, x_max_value);
+	gl.uniform1f(y_min, y_min_value);
+	gl.uniform1f(y_max, y_max_value);
 	gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 }, 1000 / 60);
