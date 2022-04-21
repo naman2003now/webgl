7b282499ac00dbd60922e443d1c6fcd1980b0849
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-04-21 23:57:26 +0530

    Ray Marching Done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 var canvas = document.getElementById("canvas");
-canvas.height = 5000;
-canvas.width = 5000;
-var gl = canvas.getContext("webgl");
-canvas.height = 1000;
-canvas.width = 1000;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
-canvas.style.height = Math.min(window.innerHeight, window.innerWidth) + "px";
-canvas.style.width = Math.min(window.innerHeight, window.innerWidth) + "px";
+var gl = canvas.getContext("webgl");
+
+canvas.style.height = window.innerHeight + "px";
+canvas.style.width = window.innerWidth + "px";
 
 if (!gl) {
 	alert(`WebGL not supported`);
@@ -45,37 +44,98 @@ const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(
 	fragmentShader,
 	`
-    precision lowp int;
-    precision highp float;
+    precision mediump float;
 
-    uniform float x_min;
-    uniform float x_max;
-    uniform float y_min;
-    uniform float y_max;
+	#define PI 3.1415926535897932384626433832795
 
-    uniform vec2 u_resolution;
+	uniform vec2 uResolution;
+	uniform vec3 uSpherePos;
+	uniform float uRadius;
+	uniform vec4 uCamera;
+	uniform vec3 uLightDirection;
+
+	uniform float uBumps;
+
+	const int MAX_ITER = 1000;
+	const float HIT_SIZE = 0.0001;
+	const float MAX_DISTANCE = 10.0;
+
+	float distance_from_plane(vec3 _coordinates){
+		return _coordinates.y + 1.0;
+	}
+
+	float distance_from_sphere(vec3 _coordinates, vec3 _origin, float _radius){
+		vec3 point_on_sphere = normalize(_coordinates-uSpherePos);
+		float bumps = sin(asin(point_on_sphere.y)*25.0 ) + cos(acos(point_on_sphere.x)*25.0 );
+		bumps/=uBumps;
+		return length(_coordinates - _origin) - _radius - bumps;
+	}
+
+	float minimum_distance(vec3 _coordinates){
+
+		return min(distance_from_plane(_coordinates), distance_from_sphere(_coordinates, uSpherePos, uRadius));
+	}
+
+	vec3 march(vec3 _coordinates, vec3 _camera, vec3 _frag, float _distance){
+		return _coordinates - _distance*normalize(_camera - _frag);
+	}
+
+	vec3 normal(vec3 _coordinates){
+		float d = minimum_distance(_coordinates);
+		float x = d - minimum_distance(_coordinates + vec3(0.00001, 0, 0));
+		float y = d - minimum_distance(_coordinates + vec3(0, 0.00001, 0));
+		float z = d - minimum_distance(_coordinates + vec3(0, 0, 0.00001));
+		return normalize(vec3(x, y, z));
+	}
+
+	int march_shadow(vec3 _coordinates){
+		float marchDistance;
+		vec3 camera = _coordinates;
+		if(_coordinates.y + 1.0 > 0.01){
+			return 0;
+		}
+		for(int iter = 0; iter < MAX_ITER; iter++){
+			marchDistance = distance_from_sphere(_coordinates, uSpherePos, uRadius);
+			if(marchDistance < HIT_SIZE){
+				return 1;
+			}
+			else if(marchDistance > MAX_DISTANCE){
+				return 0;
+			}
+			_coordinates = march(_coordinates, vec3(0, 0, 0) , uLightDirection , marchDistance);
+		}
+		return 0;
+	}
 
     void main(){
-        float x0 = x_min + (x_max - x_min)*(gl_FragCoord.x / u_resolution.x);
-        float y0 = y_min + (y_max - y_min)*(gl_FragCoord.y / u_resolution.y);
-        float x = 0.0;
-        float y = 0.0;
-        int found = 0;
-        const int max_iteration = 300;
-        for(int k = 0; k < max_iteration; k++){
-            float x_new = x*x - y*y + x0;
-            float y_new = 2.0*x*y + y0;
-            if(x_new*x_new + y_new*y_new > 4.0){
-                found = 1;
-                gl_FragColor = vec4(k > 100? k > 200? k - 100 : k: 0, k > 200? k - 200: 0,k > 100? k > 200? (k - 200) : (k - 100): k, 100)/50.0;
-                break;
-            }
-            x = x_new;
-            y = y_new;
-        }
-        if(found == 0){
-            gl_FragColor = vec4(0, 0, 0, 1);
-        }
+		vec2 uv = (gl_FragCoord.xy- 0.5*uResolution)/min(uResolution.y, uResolution.x);
+		gl_FragColor = vec4(minimum_distance(vec3(uv, 0))*vec3(1, 1, 1), 1);
+
+		float marchDistance = 0.0;
+		int hit = 0;
+		vec3 coordinates = uCamera.xyz;
+		vec3 frag = vec3(uv, uCamera.w);
+
+		for(int iter = 0; iter < MAX_ITER; iter++){
+			marchDistance = minimum_distance(coordinates);
+			if(marchDistance < HIT_SIZE){
+				hit = 1;
+				break;
+			}
+			else if(marchDistance > MAX_DISTANCE){
+				break;
+			}
+			coordinates = march(coordinates, uCamera.xyz, frag, marchDistance);
+		}
+		if(hit == 1){
+			float intensity = dot(-normalize(uLightDirection), normal(coordinates))/1.2;
+			gl_FragColor = vec4(intensity*vec3(1, 1, 1) + vec3(0.1, 0.1, 0.1), 1);
+			if(march_shadow(coordinates) == 1){
+				gl_FragColor = vec4(0.1, 0.1, 0.1, 1);
+			}
+		}else{
+			gl_FragColor = vec4(0.2, 0.2, 0.2 ,1);
+		}
     }
 `
 );
@@ -101,83 +161,29 @@ gl.useProgram(program);
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 console.log(gl.getShaderInfoLog(fragmentShader));
 
-const mouseUniformLocation = gl.getUniformLocation(program, "uMouse");
-const timeUniformLocation = gl.getUniformLocation(program, "uTime");
+//Getting Uniform Locations
+const uResolution = gl.getUniformLocation(program, "uResolution");
+const uSpherePos = gl.getUniformLocation(program, "uSpherePos");
+const uRadius = gl.getUniformLocation(program, "uRadius");
+const uCamera = gl.getUniformLocation(program, "uCamera");
+const uLightDirection = gl.getUniformLocation(program, "uLightDirection");
+const uBumps = gl.getUniformLocation(program, "uBumps");
 
-const x_min = gl.getUniformLocation(program, "x_min");
-const x_max = gl.getUniformLocation(program, "x_max");
-const y_min = gl.getUniformLocation(program, "y_min");
-const y_max = gl.getUniformLocation(program, "y_max");
-
-const resolutionUniformLocation = gl.getUniformLocation(
-	program,
-	"u_resolution"
-);
-
-gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
-
-document.getElementById("resolution").addEventListener("change", (e) => {
-	const resolution = e.target.value;
-	document.getElementById("resolutionDisplay").innerHTML = resolution;
-	canvas.height = resolution;
-	canvas.width = resolution;
-	gl.uniform2f(resolutionUniformLocation, resolution, resolution);
-});
-
-var moveDelta = 0.1;
-
-window.addEventListener("keydown", (e) => {
-	if (e.key === "ArrowLeft" || e.key === "a") {
-		x_min_value -= moveDelta;
-		x_max_value -= moveDelta;
-	} else if (e.key === "ArrowRight" || e.key === "d") {
-		x_min_value += moveDelta;
-		x_max_value += moveDelta;
-	} else if (e.key === "ArrowUp" || e.key === "w") {
-		y_min_value += moveDelta;
-		y_max_value += moveDelta;
-	} else if (e.key === "ArrowDown" || e.key === "s") {
-		y_min_value -= moveDelta;
-		y_max_value -= moveDelta;
-	} else if (e.key === "q") {
-		let scalex = (x_max_value - x_min_value) * 0.8;
-		let scaley = (y_max_value - y_min_value) * 0.8;
-
-		moveDelta *= 0.6;
-
-		let temp_x = x_max_value;
-		let temp_y = y_max_value;
-
-		x_max_value = x_min_value + scalex;
-		x_min_value = temp_x - scalex;
-
-		y_max_value = y_min_value + scaley;
-		y_min_value = temp_y - scaley;
-	} else if (e.key === "e") {
-		let scalex = (x_max_value - x_min_value) * 1.2;
-		let scaley = (y_max_value - y_min_value) * 1.2;
-
-		moveDelta *= 1.4;
-
-		let temp_x = x_max_value;
-		let temp_y = y_max_value;
-
-		x_max_value = x_min_value + scalex;
-		x_min_value = temp_x - scalex;
-
-		y_max_value = y_min_value + scaley;
-		y_min_value = temp_y - scaley;
-	}
-});
-var x_min_value = -2.0;
-var x_max_value = 0.47;
-var y_min_value = -1.12;
-var y_max_value = 1.12;
+//Setting Uniform Locations
+gl.uniform1f(uRadius, 0.85);
+gl.uniform2f(uResolution, canvas.width, canvas.height);
+gl.uniform3f(uSpherePos, 0, 0, 3);
+gl.uniform4f(uCamera, 0, 0, -1, 0);
+gl.uniform3f(uLightDirection, 1, 1, 0);
+gl.uniform1f(uBumps, 50.0);
 
 var renderLoop = setInterval(() => {
-	gl.uniform1f(x_min, x_min_value);
-	gl.uniform1f(x_max, x_max_value);
-	gl.uniform1f(y_min, y_min_value);
-	gl.uniform1f(y_max, y_max_value);
+	gl.uniform3f(
+		uLightDirection,
+		Math.sin(Date.now() / 1000),
+		3,
+		Math.cos(Date.now() / 1000)
+	);
+	gl.uniform1f(uBumps, 180 - Math.sin(Date.now() / 2000) * 150);
 	gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 }, 1000 / 60);
